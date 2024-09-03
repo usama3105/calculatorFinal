3ef86fb3526a2e68317993bf5192555ac9e7e0c8
usama3105 <um354626@gmail.com>
2024-09-03 19:27:44 +0500

    essage

---
+++ index.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+// Install the necessary dependencies:
+// npm install inquirer
+
+import inquirer from 'inquirer';
+
+async function main() {
+    const { firstNumber, operator, secondNumber } = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'firstNumber',
+            message: 'Enter the first number:',
+        },
+        {
+            type: 'list',
+            name: 'operator',
+            message: 'Select an operation:',
+            choices: ['+', '-', '*', '/'],
+        },
+        {
+            type: 'number',
+            name: 'secondNumber',
+            message: 'Enter the second number:',
+        },
+    ]);
+
+    let result: number;
+
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            console.log('Invalid operator.');
+            return;
+    }
+
+    console.log(`Result: ${result}`);
+}
+
+main();
